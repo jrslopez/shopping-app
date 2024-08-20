095f26e002ac5ea682e5e983b1aee34a8a79cdc1
James Lopez <jamesrussel.lopez@cix.csi.cuny.edu>
2024-08-20 16:45:38 -0400

    Fixed header

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="ui fixed menu">
       <div className=" ui container center">
-        <h2>Add to Cart-tastrophe</h2>
+        <h2>Add to Cart-tastrophe!</h2>
       </div>
     </div>
   )

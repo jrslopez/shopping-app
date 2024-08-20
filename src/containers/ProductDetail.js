ab6c2fc8e05b5188ebe75e39452ab65f68347bd5
James Lopez <jamesrussel.lopez@cix.csi.cuny.edu>
2024-08-20 14:44:03 -0400

    added reducer for individual products

---
+++ src/containers/ProductDetail.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react"
+import { useParams } from "react-router-dom"
+import axios from "axios"
+import { useDispatch, useSelector } from "react-redux"
+import { selectedProduct } from "../redux/actions/productActions"
+
+const ProductDetail = () => {
+  const product = useSelector((state) => state.product)
+  const { productId } = useParams()
+  const dispatch = useDispatch()
+
+  console.log(product)
+
+  const fetchProductDetail = async () => {
+    const response = await axios
+      .get(`https://fakestoreapi.com/products/${productId}`)
+      .catch((err) => {
+        console.log("error", err)
+      })
+
+    dispatch(selectedProduct(response.data))
+  }
+
+  useEffect(() => {
+    fetchProductDetail()
+  }, [])
+
+  return (
+    <div>
+      <h1>ProductDetail</h1>
+    </div>
+  )
+}
+
+export default ProductDetail
